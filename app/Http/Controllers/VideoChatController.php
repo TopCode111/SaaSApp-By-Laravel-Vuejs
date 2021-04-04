0be966373c56f7b96241073b2d0d15a82c522673
topcode111 <aldahir.martinez9197@gmail.com>
2021-04-05 07:32:25 +0800

    fixing

--- app/Http/Controllers/VideoChatController.php
+++ app/Http/Controllers/VideoChatController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Pusher\Pusher;
 
+///
 class VideoChatController extends Controller
 {
     public function index(Request $request) {
