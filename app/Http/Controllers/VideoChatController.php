9334639f3605fe7f0e0d35d330ab0d13181d29dd
topcode111 <aldahir.martinez9197@gmail.com>
2021-04-06 08:32:51 +0800

    updating

--- app/Http/Controllers/VideoChatController.php
+++ app/Http/Controllers/VideoChatController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Pusher\Pusher;
 
-///
 class VideoChatController extends Controller
 {
     public function index(Request $request) {
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+    //return view('welcome');
+//});
 
 
 Route::group(['middleware' => 'auth'], function(){
